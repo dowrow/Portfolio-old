d674b1c0f4ede4c97eaa4e291ae17ffdc701f46c
Dowrow <dowrow@Dowrow-PC.(none)>
2015-08-30 17:05:28 +0200

    Eliminador duplicados del arbol git

--- js/app.js
+++
@@ -1,8 +0,0 @@
-$(document).foundation();
-
-$('a').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 1000);
-    return false;
-});
